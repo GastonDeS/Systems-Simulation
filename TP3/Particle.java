a6ebe0b63afd897d8420c92e82a068a8595215b9
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-09-17 14:12:36 -0300

    feat: step four save state

--- TP3/Particle.java
+++ TP3/Particle.java
@@ -73,7 +73,15 @@ public class Particle {
         }
         return tc;
     }
-    
+
+    @Override
+    public String toString() {
+        return posX +
+                "," + posY +
+                "," + velX +
+                "," + velY;
+    }
+
     public double getMass() {
         return mass;
     }

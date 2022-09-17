ebcff192096a1725676c964b113182004d7be02b
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-09-17 12:24:53 -0300

    feat: TP3 Particle

---
+++ TP3/Particle.java
@@ -0,0 +1,22 @@
+
+
+public class Particle {
+
+    private final float mass;
+    private final float radius;
+    private float posX;
+    private float posY;
+    private float velX;
+    private float velY;
+
+    public Particle(float mass, float radius, float posX, float posY, float velX, float velY) {
+        this.mass = mass;
+        this.radius = radius;
+        this.posX = posX;
+        this.posY = posY;
+        this.velX = velX;
+        this.velY = velY;
+    }
+
+
+}

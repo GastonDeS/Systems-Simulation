ec44e5b71a66d06cffd81361d06b15f4a058da8f
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2022-09-17 12:49:42 -0300

    feat: distance and isOverlap

--- TP3/Particle.java
+++ TP3/Particle.java
@@ -2,14 +2,14 @@
 
 public class Particle {
 
-    private final float mass;
-    private final float radius;
-    private float posX;
-    private float posY;
-    private float velX;
-    private float velY;
+    private final double mass;
+    private final double radius;
+    private double posX;
+    private double posY;
+    private double velX;
+    private double velY;
 
-    public Particle(float mass, float radius, float posX, float posY, float velX, float velY) {
+    public Particle(double mass, double radius, double posX, double posY, double velX, double velY) {
         this.mass = mass;
         this.radius = radius;
         this.posX = posX;
@@ -18,5 +18,47 @@ public class Particle {
         this.velY = velY;
     }
 
+    public double getMass() {
+        return mass;
+    }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public double getVelX() {
+        return velX;
+    }
+
+    public double getVelY() {
+        return velY;
+    }
+
+    public Double distance(Particle particle) {
+        return Math.sqrt(Math.pow(this.posX - particle.posX, 2) + Math.pow(this.posY - particle.posY, 2));
+    }
+
+    // this has a problem on precision example 1.400000000000000004 compared to 1.4
+    public boolean isOverlap(Particle particle2) {
+        if (this.distance(particle2) <= this.radius + particle2.radius) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
 }
